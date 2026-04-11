f69a6dcfe9f90570667d33fdbce215249bfd8991
Ibeabuchi Udochukwu Miracle <chikimiracle2000@gmail.com>
2026-04-11 11:03:17 +0100

    feat: initialize TanStack Router setup and add tanstack router best practice skill

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
+import { defineConfig } from "vite"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: ["babel-plugin-react-compiler"],
-    }
-  }), tailwindcss()],
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      }
+    }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
